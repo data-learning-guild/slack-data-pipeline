5c0bcb2b430cfa75bce35083459aa0db40507a7a
sota_masuda <sota.m777121@gmail.com>
2021-03-31 16:29:16 +0900

    sample for slack-bolt

--- slack-to-lake/function_ingest/ingest.py
+++ slack-to-lake/function_ingest/ingest.py
@@ -1,8 +1,18 @@
 import logging
+import os
 
 logging.basicConfig(level=logging.DEBUG)
 
 from slack_bolt import App
 
-# process_before_response must be True when running on FaaS
-app = App(process_before_response=True)
+# ボットトークンと署名シークレットを使ってアプリを初期化します
+app = App(
+    # process_before_response must be True when running on FaaS
+    process_before_response=True,
+    token=os.environ.get("SLACK_BOT_TOKEN"),
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
+)
+
+# run app
+if __name__ == "__main__":
+    app.start(port=int(os.environ.get("PORT", 3000)))
